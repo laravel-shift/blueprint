02cbbd224b0accb5ba79a1b1cc72db608eb0f327
Jason McCreary <jason@pureconcepts.net>
2020-03-04 15:26:30 -0500

    fix: mismatched key casing (#70)

--- tests/fixtures/factories/post.php
+++ tests/fixtures/factories/post.php
@@ -9,6 +9,7 @@ $factory->define(Post::class, function (Faker $faker) {
     return [
         'title' => $faker->sentence(4),
         'author_id' => factory(\App\Author::class),
+        'author_bio' => $faker->text,
         'content' => $faker->paragraphs(3, true),
         'published_at' => $faker->dateTime(),
         'word_count' => $faker->randomNumber(),
