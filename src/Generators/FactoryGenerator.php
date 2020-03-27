1770652ca3b01608b9636ce4ce28bfe6c73f7586
Justin W <justinwhite15@gmail.com>
2020-03-27 14:13:22 -0700

    Updates to FactoryGeneratorTest

--- src/Generators/FactoryGenerator.php
+++ src/Generators/FactoryGenerator.php
@@ -28,6 +28,10 @@ class FactoryGenerator implements Generator
         foreach ($tree['models'] as $model) {
             $path = $this->getPath($model);
 
+            if (!$this->files->exists(dirname($path))) {
+                $this->files->makeDirectory(dirname($path), 0755, true);
+            }
+
             $this->files->put($path, $this->populateStub($stub, $model));
 
             $output['created'][] = $path;
