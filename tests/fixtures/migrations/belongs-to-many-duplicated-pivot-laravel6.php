2a83f572d662a65104120c664b5f9e5e28deec1c
Gianluca Bine <gian_bine@hotmail.com>
2020-05-01 15:28:23 -0300

    Added tests for duplicated pivot table generation

---
+++ tests/fixtures/migrations/belongs-to-many-duplicated-pivot-laravel6.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompanyPersonTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('company_person', function (Blueprint $table) {
+            $table->unsignedBigInteger('company_id');
+            $table->unsignedBigInteger('person_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('company_person');
+    }
+}
