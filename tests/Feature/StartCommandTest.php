d4afcad6bf9bd519b43abbe5d3d07618e117bb44
Len Woodward <gopher@Lens-iMac.local>
2020-05-05 16:31:41 -0700

    Tests Passing - possible false positive?

--- tests/Feature/StartCommandTest.php
+++ tests/Feature/StartCommandTest.php
@@ -15,18 +15,36 @@ class StartCommandTest extends TestCase
     /**
      * @test
      */
-    public function it_creates_a_draft_file_if_none_exists_then_runs_trace_command()
+    public function it_creates_a_draft_file_from_stub_if_none_exists()
     {
         Filesystem::shouldReceive('exists')
         ->andReturn(false);
 
-        Filesystem::expects('stub')
+        Filesystem::shouldReceive('stub')
         ->with('draft.stub')
         ->andReturn('stub');
 
-        Filesystem::expects('put')
+        Filesystem::shouldReceive('put')
         ->with('draft.yaml', 'stub');
 
-        Artisan::call('blueprint:start', [], new NullOutput);
+        Artisan::call('blueprint:start');
     }
+
+    /**
+     * @test
+     */
+    public function it_does_not_create_a_draft_file_if_one_exists_already()
+    {
+        Filesystem::shouldReceive('exists')
+        ->andReturn(true);
+
+        Filesystem::shouldNotReceive('put');
+
+        Artisan::call('blueprint:start');
+    }
+
+    /**
+     * @test
+     */
+    // public function it_runs_the_trace_command_
 }
