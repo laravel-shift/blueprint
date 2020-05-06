d08c2c8fb8ece098b0e7e8c4c3a42c59cc0a6a48
Gianluca Bine <gian_bine@hotmail.com>
2020-05-05 21:12:34 -0300

    Fix incorrect imports when using a custom namespace for models

---
+++ tests/fixtures/controllers/custom-models-namespace.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\TagStoreRequest;
+use App\Http\Requests\TagUpdateRequest;
+use App\Http\Resources\Tag as TagResource;
+use App\Http\Resources\TagCollection;
+use App\Models\Tag;
+use Illuminate\Http\Request;
+
+class TagController extends Controller
+{
+    /**
+     * @param \Illuminate\Http\Request $request
+     * @return \App\Http\Resources\TagCollection
+     */
+    public function index(Request $request)
+    {
+        $tags = Tag::all();
+
+        return new TagCollection($tags);
+    }
+
+    /**
+     * @param \App\Http\Requests\TagStoreRequest $request
+     * @return \App\Http\Resources\Tag
+     */
+    public function store(TagStoreRequest $request)
+    {
+        $tag = Tag::create($request->all());
+
+        return new TagResource($tag);
+    }
+
+    /**
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Models\Tag $tag
+     * @return \App\Http\Resources\Tag
+     */
+    public function show(Request $request, Tag $tag)
+    {
+        return new TagResource($tag);
+    }
+
+    /**
+     * @param \App\Http\Requests\TagUpdateRequest $request
+     * @param \App\Models\Tag $tag
+     * @return \App\Http\Resources\Tag
+     */
+    public function update(TagUpdateRequest $request, Tag $tag)
+    {
+        return new TagResource($tag);
+    }
+
+    /**
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Models\Tag $tag
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Request $request, Tag $tag)
+    {
+        $tag->delete();
+
+        return response()->noContent(200);
+    }
+}
---
+++ tests/fixtures/models/custom-models-namespace.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Tag extends Model
+{
+    use SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'icon',
+        'active',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'id' => 'integer',
+        'active' => 'boolean',
+    ];
+}
