7954a452068934ce3854d1325a4f1fcb74bbf9fd
Nathan E <nathane@users.noreply.github.com>
2020-08-26 15:41:16 -0400

    chore: add test fixtures

---
+++ tests/fixtures/migrations/columns-with-comments.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProfessionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('professions', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 400)->comment("Some title for the profession");
+            $table->string('description', 400)->nullable()->comment('Some description for the profession');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('professions');
+    }
+}
