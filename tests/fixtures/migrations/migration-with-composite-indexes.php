0865c8e872b88716612428c5f7222cbacf03c918
Nathan E <nathane@users.noreply.github.com>
2020-08-26 18:00:05 -0400

    chore: add test fixtures

---
+++ tests/fixtures/migrations/migration-with-composite-indexes.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateParticipationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('participations', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('project_id');
+            $table->bigInteger('user_id');
+            $table->string('name');
+            $table->unique(['project_id', 'user_id']);
+            $table->index(['project_id', 'user_id']);
+            $table->primary(['project_id', 'user_id']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('participations');
+    }
+}
