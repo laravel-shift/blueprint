3371d1e289ab2fe4733f54a1648f5a75c5598fc0
Nathan E <nathane@users.noreply.github.com>
2020-08-30 02:02:58 -0400

    chore: clean up

--- tests/Traits/MocksFilesystem.php
+++ tests/Traits/MocksFilesystem.php
@@ -6,7 +6,7 @@ trait MocksFilesystem
     protected function setUp():void
     {
         parent::setUp();
-        $this->files = $this->filesystem = $this->mock(\Illuminate\Filesystem\Filesystem::class);
+        $this->files = $this->mock(\Illuminate\Filesystem\Filesystem::class);
         $this->swap('files', $this->files);
     }
 }
