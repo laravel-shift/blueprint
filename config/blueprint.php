a2a1aa430b4113b2c076bf09bc8d52210f843c83
Nathan E <nathane@users.noreply.github.com>
2020-08-29 22:06:47 -0400

    feat: support custom generators via config

--- config/blueprint.php
+++ config/blueprint.php
@@ -116,4 +116,33 @@ return [
     */
     'generate_fqcn_route' => false,
 
+    /*
+    |--------------------------------------------------------------------------
+    | Generators
+    |--------------------------------------------------------------------------
+    |
+    | Blueprint will automatically register this default array of Generator classes.
+    | However, you may configure Blueprint to use a custom Generator class to grant
+    | expanded functionality to Blueprint.
+    |
+    | Your custom Generator class MUST implement the 'Blueprint\Contracts\Generator' interface.
+    |
+    */
+    'generators' => [
+        'controller' => \Blueprint\Generators\ControllerGenerator::class,
+        'factory' => \Blueprint\Generators\FactoryGenerator::class,
+        'migration' => \Blueprint\Generators\MigrationGenerator::class,
+        'model' => \Blueprint\Generators\ModelGenerator::class,
+        'route' => \Blueprint\Generators\RouteGenerator::class,
+        'seeder' => \Blueprint\Generators\SeederGenerator::class,
+        'test' => \Blueprint\Generators\TestGenerator::class,
+        'event' => \Blueprint\Generators\Statements\EventGenerator::class,
+        'form_request' => \Blueprint\Generators\Statements\FormRequestGenerator::class,
+        'job' => \Blueprint\Generators\Statements\JobGenerator::class,
+        'mail' => \Blueprint\Generators\Statements\MailGenerator::class,
+        'notification' => \Blueprint\Generators\Statements\NotificationGenerator::class,
+        'resource' => \Blueprint\Generators\Statements\ResourceGenerator::class,
+        'view' => \Blueprint\Generators\Statements\ViewGenerator::class,
+    ],
+
 ];
