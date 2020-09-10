fea394a4dbb1eb3516110566522f23ed5d26cab7
Nathan E <nathane@users.noreply.github.com>
2020-09-10 12:53:02 -0400

    chore: update fixtures

---
+++ tests/fixtures/tests/reference-cache-laravel-8.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers;
+
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use JMac\Testing\Traits\AdditionalAssertions;
+use Tests\TestCase;
+
+/**
+ * @see \App\Http\Controllers\UserController
+ */
+class UserControllerTest extends TestCase
+{
+    use AdditionalAssertions, RefreshDatabase, WithFaker;
+
+    /**
+     * @test
+     */
+    public function store_uses_form_request_validation()
+    {
+        $this->assertActionUsesFormRequest(
+            \App\Http\Controllers\UserController::class,
+            'store',
+            \App\Http\Requests\UserControllerStoreRequest::class
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function store_saves_and_redirects()
+    {
+        $email = $this->faker->safeEmail;
+        $password = $this->faker->password;
+
+        $response = $this->post(route('user.store'), [
+            'email' => $email,
+            'password' => $password,
+        ]);
+
+        $users = User::query()
+            ->where('email', $email)
+            ->where('password', $password)
+            ->get();
+        $this->assertCount(1, $users);
+        $user = $users->first();
+
+        $response->assertRedirect(route('dashboard'));
+        $response->assertSessionHas('user.email', $user->email);
+    }
+}
