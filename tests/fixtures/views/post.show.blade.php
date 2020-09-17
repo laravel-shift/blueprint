03692fef22cf083efdba374c01da85ff8ef9c683
Jason McCreary <jason@pureconcepts.net>
2020-09-17 19:42:39 -0400

    Fix Blade comment syntax

--- tests/fixtures/views/post.show.blade.php
+++ tests/fixtures/views/post.show.blade.php
@@ -1,7 +1,7 @@
-{--
+{{--
     @extends('layouts.app')
 
     @section('content')
         post.show template
     @endsection
---}
+--}}
--- tests/fixtures/views/user.index.blade.php
+++ tests/fixtures/views/user.index.blade.php
@@ -1,7 +1,7 @@
-{--
+{{--
     @extends('layouts.app')
 
     @section('content')
         user.index template
     @endsection
---}
+--}}
