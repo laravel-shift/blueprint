9b957be591f9cffe70fbed8f560da28550d49ee2
Nathan E <nathane@users.noreply.github.com>
2020-12-05 11:54:30 -0500

    chore: add fixtures

---
+++ tests/fixtures/models/uuid-shorthand-invalid-relationship.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AgeCohort extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'description',
+        'min_age',
+        'max_age',
+        'uuid',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'id' => 'integer',
+    ];
+}
