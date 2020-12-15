d9b91b468e066bf23fc2d92d63fe2f4b93a16b06
Nathan E <nathane@users.noreply.github.com>
2020-12-15 17:58:02 -0500

    feat(FactoryGenerator): methods return types

---
+++ tests/fixtures/factories/return-type-declarations-laravel8.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+use App\Post;
+use App\User;
+
+class PostFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Post::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->sentence(4),
+            'content' => $this->faker->paragraphs(3, true),
+            'published_at' => $this->faker->dateTime(),
+            'author_id' => User::factory(),
+        ];
+    }
+}
