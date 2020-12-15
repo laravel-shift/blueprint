edca44b3414d016ab1669cce8e0e8ba79f30ee7c
Nathan E <nathane@users.noreply.github.com>
2020-12-15 18:10:42 -0500

    feat(ResourceGenerator): methods return types

---
+++ tests/fixtures/resources/user-return-type-declarations.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+        ];
+    }
+}
