7d39437e4e1ab9ba741a2db23dcec10e419ea5f5
TinaH <tinahammar@gmail.com>
2021-01-28 12:26:13 +0100

    Add Morphs and uuidMorphs php annotation
    fixes #422 
    Don't know if you prefer a switch statement or elseif

--- src/Generators/ModelGenerator.php
+++ src/Generators/ModelGenerator.php
@@ -91,8 +91,20 @@ class ModelGenerator implements Generator
         $phpDoc .= PHP_EOL;
         /** @var Column $column */
         foreach ($model->columns() as $column) {
-            $phpDoc .= sprintf(' * @property %s $%s', $this->phpDataType($column->dataType()), $column->name());
-            $phpDoc .= PHP_EOL;
+            if ($column->dataType() === 'morphs') {
+                $phpDoc .= ' * @property int $'. $column->name() . '_id';
+                $phpDoc .= PHP_EOL;
+                $phpDoc .= ' * @property string $'. $column->name() . '_type';
+                $phpDoc .= PHP_EOL;
+            } elseif ($column->dataType() === 'uuidMorphs') {
+                $phpDoc .= ' * @property string $'. $column->name() . '_id';
+                $phpDoc .= PHP_EOL;
+                $phpDoc .= ' * @property string $'. $column->name() . '_type';
+                $phpDoc .= PHP_EOL;
+            } else {
+                $phpDoc .= sprintf(' * @property %s $%s', $this->phpDataType($column->dataType()), $column->name());
+                $phpDoc .= PHP_EOL;
+            }
         }
 
         if ($model->usesSoftDeletes()) {
