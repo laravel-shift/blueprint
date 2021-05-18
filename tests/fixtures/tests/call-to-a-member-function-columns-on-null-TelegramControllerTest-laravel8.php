26886d9a7b958c05f126f5a6ab792a443904a883
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2021-05-18 17:01:44 -0400

    Add tests and fixtures
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/fixtures/tests/call-to-a-member-function-columns-on-null-TelegramControllerTest-laravel8.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers;
+
+use Tests\TestCase;
+
+/**
+ * @see \App\Http\Controllers\TelegramController
+ */
+class TelegramControllerTest extends TestCase
+{
+    
+}
