6331d4fac00bd0bfafde27b288b91cf6bc1af75a
Pedro X <pxpm88@gmail.com>
2021-07-22 14:05:40 +0100

    allow non-id columns to be nullable when using foreign

---
+++ tests/fixtures/migrations/nullable-columns-with-foreign.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::disableForeignKeyConstraints();
+
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->nullable()->constrained();
+            $table->integer('integer')->nullable();
+            $table->foreign('integer')->references('id')->on('monsteras');
+            $table->timestamps();
+        });
+
+        Schema::enableForeignKeyConstraints();
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
