5402a57169890d3da2f59d022b6890e69bf52169
Sajad Dehshiri <sajaddp71@gmail.com>
2022-09-04 15:09:48 +0430

    update tests

--- tests/fixtures/migrations/nullable-chaining.php
+++ tests/fixtures/migrations/nullable-chaining.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCartsTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -34,4 +34,4 @@ class CreateCartsTable extends Migration
     {
         Schema::dropIfExists('carts');
     }
-}
+};
