2f3e81ae1fba9674ca1733936eaa30a82d3b624f
Jason McCreary <jason@pureconcepts.net>
2023-03-01 13:31:49 -0500

    Update mailable template

--- tests/fixtures/mailables/added-admin.php
+++ tests/fixtures/mailables/added-admin.php
@@ -5,6 +5,8 @@ namespace App\Mail;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
 class AddedAdmin extends Mailable
@@ -22,10 +24,32 @@ class AddedAdmin extends Mailable
     }
 
     /**
-     * Build the message.
+     * Get the message envelope.
      */
-    public function build(): static
+    public function envelope(): Envelope
     {
-        return $this->view('emails.admin.added');
+        return new Envelope(
+            subject: '',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'emails.admin.added',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
     }
 }
--- tests/fixtures/mailables/review-post.php
+++ tests/fixtures/mailables/review-post.php
@@ -5,6 +5,8 @@ namespace App\Mail;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
 class ReviewPost extends Mailable
@@ -22,10 +24,32 @@ class ReviewPost extends Mailable
     }
 
     /**
-     * Build the message.
+     * Get the message envelope.
      */
-    public function build(): static
+    public function envelope(): Envelope
     {
-        return $this->view('emails.review-post');
+        return new Envelope(
+            subject: '',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'emails.review-post',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
     }
 }
