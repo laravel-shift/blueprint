5ac1b7e174ff63b6e3ac484b1a8fb0f7127feb28
rcrosbourne <rcrosbourne@gmail.com>
2023-12-06 23:04:00 -0500

    Add 'ulid' type to ModelLexer.php.

--- src/Generators/ModelGenerator.php
+++ src/Generators/ModelGenerator.php
@@ -272,12 +272,12 @@ class ModelGenerator extends AbstractClassGenerator implements Generator
     protected function addTraits(Model $model, $stub): string
     {
         $traits = ['HasFactory'];
-        
+
         if ($model->usesSoftDeletes()) {
             $this->addImport($model, 'Illuminate\\Database\\Eloquent\\SoftDeletes');
             $traits[] = 'SoftDeletes';
         }
-        
+
         if ($model->usesUuids()) {
             $this->addImport($model, 'Illuminate\\Database\\Eloquent\\Concerns\\HasUuids');
             $traits[] = 'HasUuids';
