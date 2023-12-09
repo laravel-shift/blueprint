0f57f5bad4fdd2e2363f9bfefae0e0f93930eb1b
rcrosbourne <rcrosbourne@gmail.com>
2023-12-08 23:14:37 -0500

    remove dump and linted code

--- src/Generators/ModelGenerator.php
+++ src/Generators/ModelGenerator.php
@@ -64,7 +64,6 @@ class ModelGenerator extends AbstractClassGenerator implements Generator
         $stub = $this->addTraits($model, $stub);
         $stub = str_replace('{{ imports }}', $this->buildImports($model), $stub);
 
-        dump($stub);
         return $stub;
     }
 
