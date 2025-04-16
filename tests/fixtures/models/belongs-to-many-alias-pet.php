b2d7e29e67f4ba77b05e8ed24de71bb0ff398854
Jason McCreary <jason@pureconcepts.net>
2025-04-16 10:41:13 -0400

    WIP

---
+++ tests/fixtures/models/belongs-to-many-alias-pet.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Pet extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'id' => 'integer',
+    ];
+
+    public function owners(): BelongsToMany
+    {
+        return $this->belongsToMany(Customer::class);
+    }
+}
