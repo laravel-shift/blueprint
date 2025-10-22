9b9adf9dce61418394967ba300484ecdca6ffffd
Jason McCreary <jason@pureconcepts.net>
2025-10-22 11:25:09 -0400

    Drop support for Laravel 11 (#758)

--- tests/Feature/Translators/RulesTest.php
+++ tests/Feature/Translators/RulesTest.php
@@ -14,7 +14,7 @@ use Tests\TestCase;
 final class RulesTest extends TestCase
 {
     #[Test]
-    public function forColumn_returns_required_rule_by_default(): void
+    public function for_column_returns_required_rule_by_default(): void
     {
         $column = new Column('test', 'unknown');
 
@@ -22,7 +22,7 @@ final class RulesTest extends TestCase
     }
 
     #[Test]
-    public function forColumn_returns_nullable_rule(): void
+    public function for_column_returns_nullable_rule(): void
     {
         $column = new Column('test', 'string', ['nullable']);
 
@@ -31,7 +31,7 @@ final class RulesTest extends TestCase
 
     #[Test]
     #[DataProvider('stringDataTypesProvider')]
-    public function forColumn_returns_string_rule_for_string_data_types($data_type): void
+    public function for_column_returns_string_rule_for_string_data_types($data_type): void
     {
         $column = new Column('test', $data_type);
 
@@ -39,7 +39,7 @@ final class RulesTest extends TestCase
     }
 
     #[Test]
-    public function forColumn_returns_max_rule_for_string_attributes(): void
+    public function for_column_returns_max_rule_for_string_attributes(): void
     {
         $column = new Column('test', 'string', [], [1000]);
 
@@ -52,7 +52,7 @@ final class RulesTest extends TestCase
 
     #[Test]
     #[DataProvider('stringDataTypesProvider')]
-    public function forColumn_uses_email_rule_for_columns_named_email_or_email_address($data_type): void
+    public function for_column_uses_email_rule_for_columns_named_email_or_email_address($data_type): void
     {
         $column = new Column('email', $data_type);
 
@@ -67,7 +67,7 @@ final class RulesTest extends TestCase
 
     #[Test]
     #[DataProvider('stringDataTypesProvider')]
-    public function forColumn_uses_password_rule_for_columns_named_password($data_type): void
+    public function for_column_uses_password_rule_for_columns_named_password($data_type): void
     {
         $column = new Column('password', $data_type);
 
@@ -77,7 +77,7 @@ final class RulesTest extends TestCase
 
     #[Test]
     #[DataProvider('numericDataTypesProvider')]
-    public function forColumn_returns_numeric_rule_for_numeric_types($data_type): void
+    public function for_column_returns_numeric_rule_for_numeric_types($data_type): void
     {
         $column = new Column('test', $data_type);
 
@@ -86,14 +86,14 @@ final class RulesTest extends TestCase
 
     #[Test]
     #[DataProvider('integerDataTypesProvider')]
-    public function forColumn_returns_integer_rule_for_integer_types($data_type): void
+    public function for_column_returns_integer_rule_for_integer_types($data_type): void
     {
         $column = new Column('test', $data_type);
         $this->assertContains('integer', Rules::fromColumn('context', $column));
     }
 
     #[Test]
-    public function forColumn_return_exists_rule_for_id_column(): void
+    public function for_column_return_exists_rule_for_id_column(): void
     {
         $column = new Column('user_id', 'id');
 
@@ -101,7 +101,7 @@ final class RulesTest extends TestCase
     }
 
     #[Test]
-    public function forColumn_return_exists_rule_id_column_with_attribute(): void
+    public function for_column_return_exists_rule_id_column_with_attribute(): void
     {
         $column = new Column('author_id', 'id', [], ['user']);
 
@@ -109,7 +109,7 @@ final class RulesTest extends TestCase
     }
 
     #[Test]
-    public function forColumn_return_exists_rule_for_the_unique_modifier(): void
+    public function for_column_return_exists_rule_for_the_unique_modifier(): void
     {
         $column = new Column('column', 'string', ['unique']);
 
@@ -118,7 +118,7 @@ final class RulesTest extends TestCase
 
     #[Test]
     #[DataProvider('relationshipColumnProvider')]
-    public function forColumn_returns_exists_rule_for_foreign_keys($name, $table): void
+    public function for_column_returns_exists_rule_for_foreign_keys($name, $table): void
     {
         $column = new Column($name, 'id');
 
@@ -129,7 +129,7 @@ final class RulesTest extends TestCase
     }
 
     #[Test]
-    public function forColumn_returns_exists_rule_for_foreign_keys_with_foreign_table_name(): void
+    public function for_column_returns_exists_rule_for_foreign_keys_with_foreign_table_name(): void
     {
         $column = new Column('author_id', 'id', [['foreign' => 'users']]);
 
@@ -140,7 +140,7 @@ final class RulesTest extends TestCase
     }
 
     #[Test]
-    public function forColumn_returns_gt0_rule_for_unsigned_numeric_types(): void
+    public function for_column_returns_gt0_rule_for_unsigned_numeric_types(): void
     {
         $column = new Column('test', 'integer');
 
@@ -152,7 +152,7 @@ final class RulesTest extends TestCase
     }
 
     #[Test]
-    public function forColumn_returns_in_rule_for_enums_and_sets(): void
+    public function for_column_returns_in_rule_for_enums_and_sets(): void
     {
         $column = new Column('test', 'enum', [], ['alpha', 'bravo', 'charlie']);
         $this->assertContains('in:alpha,bravo,charlie', Rules::fromColumn('context', $column));
@@ -164,7 +164,7 @@ final class RulesTest extends TestCase
 
     #[Test]
     #[DataProvider('dateDataTypesProvider')]
-    public function forColumn_returns_date_rule_for_date_types($data_type): void
+    public function for_column_returns_date_rule_for_date_types($data_type): void
     {
         $column = new Column('test', $data_type);
 
@@ -172,7 +172,7 @@ final class RulesTest extends TestCase
     }
 
     #[Test]
-    public function forColumn_return_json_rule_for_the_json_type(): void
+    public function for_column_return_json_rule_for_the_json_type(): void
     {
         $column = new Column('column', 'json');
 
@@ -180,7 +180,7 @@ final class RulesTest extends TestCase
     }
 
     #[Test]
-    public function forColumn_does_not_return_between_rule_for_decimal_without_precion_and_scale(): void
+    public function for_column_does_not_return_between_rule_for_decimal_without_precion_and_scale(): void
     {
         $column = new Column('column', 'decimal');
 
@@ -188,7 +188,7 @@ final class RulesTest extends TestCase
     }
 
     #[Test]
-    public function forColumn_does_not_return_between_rule_for_unsigned_decimal_without_precision_and_scale(): void
+    public function for_column_does_not_return_between_rule_for_unsigned_decimal_without_precision_and_scale(): void
     {
         $unsignedBeforeDecimalColumn = new Column('column', 'unsigned decimal');
 
@@ -201,21 +201,21 @@ final class RulesTest extends TestCase
 
     #[Test]
     #[DataProvider('noBetweenRuleDataProvider')]
-    public function forColumn_does_not_return_between_rule_for_double_without_precision_and_scale($column): void
+    public function for_column_does_not_return_between_rule_for_double_without_precision_and_scale($column): void
     {
         $this->assertNotContains('between', Rules::fromColumn('context', $column));
     }
 
     #[Test]
     #[DataProvider('noBetweenRuleDataProvider')]
-    public function forColumn_does_not_return_between_rule($column): void
+    public function for_column_does_not_return_between_rule($column): void
     {
         $this->assertNotContains('between', Rules::fromColumn('context', $column));
     }
 
     #[Test]
     #[DataProvider('betweenRuleDataProvider')]
-    public function forColumn_returns_between_rule($column, $interval): void
+    public function for_column_returns_between_rule($column, $interval): void
     {
         $fromColumn = Rules::fromColumn('context', $column);
         $this->assertContains("between:$interval", $fromColumn);
