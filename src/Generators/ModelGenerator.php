8a17fd76de3b5e2c52e8de9acd3c55732fa95ec3
Jason McCreary <jason@pureconcepts.net>
2026-09-24 12:50:41 -0400

    Fix decimal cast without scale

--- src/Generators/ModelGenerator.php
+++ src/Generators/ModelGenerator.php
@@ -316,7 +316,7 @@ class ModelGenerator extends AbstractClassGenerator implements Generator
 
         if (in_array($column->dataType(), ['decimal', 'unsignedDecimal'])) {
             if ($column->attributes()) {
-                return 'decimal:' . $column->attributes()[1];
+                return 'decimal:' . ($column->attributes()[1] ?? 2);
             }
 
             return 'decimal';
---
+++ tests/fixtures/models/decimal-precision.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Invoice extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'subtotal',
+        'total',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'id' => 'integer',
+            'subtotal' => 'decimal:2',
+            'total' => 'decimal:4',
+        ];
+    }
+}
